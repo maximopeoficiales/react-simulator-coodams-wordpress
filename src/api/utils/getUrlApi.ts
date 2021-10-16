e5a229c2cedfcd7e3a37500f6389ded7f76fde72
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-10-15 21:28:52 -0500

    ADD: ultimos detalles - falta arreglar el calculado

--- src/api/utils/getUrlApi.ts
+++ src/api/utils/getUrlApi.ts
@@ -3,17 +3,18 @@ import { config } from "../../config/config";
 export const getUrlApi = () => {
     let { URL_GET_OPTIONS, URL_API_BASE } = config;
     // eslint-disable-next-line no-restricted-globals
-    let host = location.hostname;
+    let host = location.host;
     // eslint-disable-next-line no-restricted-globals
     let protocol = location.protocol;
     // eslint-disable-next-line no-restricted-globals
     let existsInUrlTest = location.href.includes("test");
 
     let urlBase = protocol + "//" + host + (existsInUrlTest ? "/test" : "");
+
     if (host.includes("localhost")) {
         urlBase = URL_API_BASE;
     }
 
-    // return `${urlBase}/${URL_GET_OPTIONS}`;
-    return `http://localhost:97/${URL_GET_OPTIONS}`;
+    return `${urlBase}/${URL_GET_OPTIONS}`;
+    // return `http://localhost:97/${URL_GET_OPTIONS}`;
 }
